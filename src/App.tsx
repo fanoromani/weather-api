4ebee201af28a70d94ffbd001462c817e941a15d
Stefano Romani <fanoromani@gmail.com>
2023-09-11 18:30:46 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,26 @@ import { useEffect, useState } from "react";
 import { Card } from "./components/Card";
 
 function App() {
-  const key = "3bd3dfaaaf5d4642a54234042222510";
   const [forecast, setForecast] = useState<any>({});
   const [showCard, setShowCard] = useState<boolean>(false);
   const [city, setCity] = useState<string>("");
 
   function handleChangeCity(event: any) {
     setCity(event.target.value);
-    console.log(city);
   }
 
-  /*  function handleShowCard(city: String) {
-    axios(
-      `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=6&aqi=no&alerts=no`
-    ).then((response) => {
-      setForecast(response.data);
-    });
-    showCard === false ? setShowCard(true) : setShowCard(false);
-  }
-  console.log(`${forecast.data} = forecast... ${city} = city`); */
-
-  const callApi = async (city: string) => {
+  const callApi = async () => {
     const response = await axios(
-      `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=6&aqi=no&alerts=no`
+      `https://api.weatherapi.com/v1/forecast.json?key=${
+        import.meta.env.VITE_API_KEY
+      }&q=${city}&days=6&aqi=no&alerts=no`
     );
     setForecast(response.data);
-    showCard === false ? setShowCard(true) : setShowCard(false);
+    setShowCard(true);
   };
 
   return (
-    <div className="flex justify-center items-start w-screen h-screen bg-gradient-to-t from-yellow-400 to-orange-500">
+    <div className="flex justify-center items-start w-screen h-screen bg-gradient-to-t from-green-400 to-blue-500">
       <div className="flex flex-col w-full sm:w-auto">
         <h1 className="sm:mt-40 text-center text-white text-5xl sm:text-6xl">
           Weather Forecast
@@ -43,7 +33,7 @@ function App() {
           <form
             onSubmit={(e) => {
               e.preventDefault();
-              callApi(city);
+              callApi();
             }}
           >
             <input
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,11 @@
 import CloseIcon from "@mui/icons-material/Close";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import { useState } from "react";
 
 export function Card({ forecast }: any) {
+  const [weekDay, setWeekDay] = useState();
+
   return (
     <div className="flex flex-col gap-4 pl-12 pr-6 py-4 sm:mt-10 h-72 w-full bg-white">
       <div className="flex justify-between">
@@ -16,11 +19,11 @@ export function Card({ forecast }: any) {
       </div>
       <div className="flex justify-between w-3/5">
         <div>
-          <ArrowDownwardIcon className="text-orange-400" />
+          <ArrowDownwardIcon className="text-blue-600" />
           <span className="font-bold">
             {forecast.forecast.forecastday[0].day.mintemp_c}°
           </span>
-          <ArrowUpwardIcon className="text-orange-400" />
+          <ArrowUpwardIcon className="text-blue-600" />
           <span className="font-bold">
             {forecast.forecast.forecastday[0].day.maxtemp_c}°
           </span>
@@ -41,43 +44,55 @@ export function Card({ forecast }: any) {
           <span className="font-bold">{forecast.current.humidity}%</span>
         </div>
       </div>
-      <div className="h-[1px] w-11/12 bg-orange-300 text-center"></div>
-      <div className="flex justify-around pr-12">
+      <div className="h-[1px] w-11/12 bg-blue-300 text-center"></div>
+      {/* <div className="flex justify-around pr-12">
         <div className="text-center">
           <div className="font-bold">Tue</div>
-          <span className="text-orange-400 font-bold">
+          <span className="text-blue-600 font-bold">
             {parseInt(forecast.forecast.forecastday[1].day.mintemp_c)}°{" "}
             {parseInt(forecast.forecast.forecastday[1].day.maxtemp_c)}°
           </span>
         </div>
         <div className="text-center">
           <div className="font-bold">Wed</div>
-          <span className="text-orange-400 font-bold">
+          <span className="text-blue-600 font-bold">
             {parseInt(forecast.forecast.forecastday[2].day.mintemp_c)}°{" "}
             {parseInt(forecast.forecast.forecastday[2].day.maxtemp_c)}°
           </span>
         </div>
         <div className="text-center">
           <div className="font-bold">Thu</div>
-          <span className="text-orange-400 font-bold">
+          <span className="text-blue-600 font-bold">
             {parseInt(forecast.forecast.forecastday[3].day.mintemp_c)}°{" "}
             {parseInt(forecast.forecast.forecastday[3].day.maxtemp_c)}°
           </span>
         </div>
         <div className="text-center">
           <div className="font-bold">Fri</div>
-          <span className="text-orange-400 font-bold">
+          <span className="text-blue-600 font-bold">
             {parseInt(forecast.forecast.forecastday[4].day.mintemp_c)}°{" "}
             {parseInt(forecast.forecast.forecastday[4].day.maxtemp_c)}°
           </span>
         </div>
         <div className="text-center">
           <div className="font-bold">Sat</div>
-          <span className="text-orange-400 font-bold">
+          <span className="text-blue-600 font-bold">
             {parseInt(forecast.forecast.forecastday[5].day.mintemp_c)}°{" "}
             {parseInt(forecast.forecast.forecastday[5].day.maxtemp_c)}°
           </span>
         </div>
+      </div> */}
+      <div className="flex justify-around pr-12">
+        {forecast.forecast.forecastday.map((day: any) => (
+          <div className="text-center" key={day.date}>
+            <div className="font-bold">{day.date.slice(5, 10)}</div>
+            <span className="text-blue-600 font-bold">
+              <ArrowDownwardIcon />
+              {parseInt(day.day.mintemp_c)}° {parseInt(day.day.maxtemp_c)}°
+              <ArrowUpwardIcon />
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   );
